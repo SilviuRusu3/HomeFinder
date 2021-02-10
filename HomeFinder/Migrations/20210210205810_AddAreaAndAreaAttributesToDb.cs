a285b80a8250811c7a252ffc06d80b847e1ffd6c
DESKTOP-UO3TM2T\Silviu <sylviu_r@yahoo.com>
2021-02-10 23:07:46 +0200

    Add Area to Db + configure many to many relation w/ LocationAttributes

---
+++ HomeFinder/Migrations/20210210205810_AddAreaAndAreaAttributesToDb.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace HomeFinder.Migrations
+{
+    public partial class AddAreaAndAreaAttributesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Areas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    GeneralGrade = table.Column<double>(type: "double precision", nullable: true),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Areas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Areas_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AreaAttributes",
+                columns: table => new
+                {
+                    LocationAttributesId = table.Column<int>(type: "integer", nullable: false),
+                    AreaId = table.Column<int>(type: "integer", nullable: false),
+                    Grade = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AreaAttributes", x => new { x.LocationAttributesId, x.AreaId });
+                    table.ForeignKey(
+                        name: "FK_AreaAttributes_Areas_AreaId",
+                        column: x => x.AreaId,
+                        principalTable: "Areas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AreaAttributes_Attributes_LocationAttributesId",
+                        column: x => x.LocationAttributesId,
+                        principalTable: "Attributes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AreaAttributes_AreaId",
+                table: "AreaAttributes",
+                column: "AreaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Areas_UserId",
+                table: "Areas",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AreaAttributes");
+
+            migrationBuilder.DropTable(
+                name: "Areas");
+        }
+    }
+}
---
+++ HomeFinder/Models/AreaAttributes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeFinder.Models
+{
+    public class AreaAttributes
+    {
+        public int LocationAttributesId { get; set; }
+        public LocationAttributes LocationAttributes { get; set; }
+        [ForeignKey("LocationAttributesId")]
+        public int AreaId { get; set; }
+        [ForeignKey("AreaId")]
+        public Area Area { get; set; }
+        [Required]
+        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        public double Grade { get; set; }
+    }
+}
